a4211b1b5c1b8852594194e525c32ed7c827dac9
bante27 <mitikubantalem@gmail.com>
2026-08-21 10:20:48 -0900

    update dash

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -33,10 +33,18 @@ const protect = async (req, res, next) => {
 };
 
 const admin = (req, res, next) => {
-  if (req.user && req.user.role === 'admin') {
+  if (req.user && (req.user.role === 'admin' || req.user.role === 'superadmin')) {
     next();
   } else {
     return res.status(403).json({ message: 'Not authorized as an admin' });
+  }
+};
+
+const superAdmin = (req, res, next) => {
+  if (req.user && req.user.role === 'superadmin') {
+    next();
+  } else {
+    return res.status(403).json({ message: 'Not authorized as a super admin' });
   }
 };
 
