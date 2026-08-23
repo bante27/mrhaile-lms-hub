2d05853f8551d47b1c56fb0ec0ef8fe0660b69f4
bante27 <mitikubantalem@gmail.com>
2026-08-23 10:36:51 -0900

    update swagger

--- config/swagger.js
+++ config/swagger.js
@@ -1,36 +1,10 @@
-const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'MrHaile.com API',
-      version: '1.0.0',
-      description: 'Backend REST API documentation for MrHaile.com LMS and Digital Asset Hub.',
-    },
-    servers: [
-      {
-        url: 'http://localhost:5000',
-        description: 'Local Development Server',
-      },
-    ],
-    components: {
-      securitySchemes: {
-        BearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
-        },
-      },
-    },
-  },
-  apis: ['./swagger.yaml'],
-};
-
-const specs = swaggerJsdoc(options);
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
 
 module.exports = {
   swaggerUi,
-  specs,
+  specs: swaggerDocument,
 };
