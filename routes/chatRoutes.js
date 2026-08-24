14ae982a6409d4792dea5846ce997245289e1a54
bante27 <mitikubantalem@gmail.com>
2026-08-24 09:01:17 -0900

    update

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -14,20 +14,14 @@ const { protect, admin } = require('../middleware/authMiddleware');
 router.route('/')
   .get(protect, getConversations);
 
-router.route('/:conversationId/messages')
-  .get(protect, getMessages)
-  .post(protect, sendMessage);
-
-router.route('/:conversationId/read')
-  .patch(protect, markConversationRead);
-
-router.route('/:conversationId/status')
-  .patch(protect, admin, updateConversationStatus);
-
 router.route('/messages/:messageId/read')
   .patch(protect, markMessageRead);
 
 router.route('/messages/:messageId/delete')
   .patch(protect, deleteMessage);
+
+router.route('/:conversationId/messages')
+  .get(protect, getMessages)
+  .post(protect, sendMessage);
 
 module.exports = router;
