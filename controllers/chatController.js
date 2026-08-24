37f41e7f9c3c8996b39acb475a28616abf74247c
bante27 <mitikubantalem@gmail.com>
2026-08-24 06:31:01 -0900

    update

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const Conversation = require('../models/Conversation');
 const Message = require('../models/Message');
 
@@ -360,4 +361,3 @@ module.exports = {
   deleteMessage,
   updateConversationStatus
 };
-
