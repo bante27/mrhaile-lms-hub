204cf533b17ff96bccf8494e0a8134e55f050fa2
bante27 <mitikubantalem@gmail.com>
2026-08-30 13:11:07 -0900

    add validationschema

--- routes/homeVideoRoutes.js
+++ routes/homeVideoRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const { getHomeVideo, createHomeVideo, updateHomeVideo } = require('../controllers/homeVideoController');
 const { protect, admin } = require('../middleware/authMiddleware');
 const upload = require('../middleware/uploadMiddleware');
+const validate = require('../middleware/validateMiddleware');
+const { homeVideoSchema } = require('../validation/homeVideoValidation');
 
 router.route('/')
   .get(getHomeVideo)
-  .post(protect, admin, upload.any(), createHomeVideo)
-  .put(protect, admin, upload.any(), updateHomeVideo);
+  .post(protect, admin, upload.any(), validate(homeVideoSchema), createHomeVideo)
+  .put(protect, admin, upload.any(), validate(homeVideoSchema), updateHomeVideo);
 
 module.exports = router;
