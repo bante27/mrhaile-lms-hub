930eac489e0800ab68d6dd0120c8d9b731508535
bante27 <mitikubantalem@gmail.com>
2026-08-31 01:02:07 -0900

    remove all try cach

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -3,35 +3,32 @@ const Course = require('../models/Course');
 const Asset = require('../models/Asset');
 const Review = require('../models/Review');
 const cache = require('../utils/fallbackCache');
+const catchAsync = require('../utils/catchAsync');
 
-const getPlatformStats = async (req, res) => {
-  try {
-    const cacheKey = 'stats:platform';
-    const cachedStats = await cache.get(cacheKey);
+const getPlatformStats = catchAsync(async (req, res, next) => {
+  const cacheKey = 'stats:platform';
+  const cachedStats = await cache.get(cacheKey);
 
-    if (cachedStats) {
-      return res.json({ source: 'cache', ...cachedStats });
-    }
-
-    const totalStudents = await User.countDocuments();
-    const totalCourses = await Course.countDocuments();
-    const totalAssets = await Asset.countDocuments();
-    const totalReviews = await Review.countDocuments();
-
-    const stats = {
-      activeStudents: totalStudents,
-      masterclasses: totalCourses,
-      digitalAssets: totalAssets,
-      totalReviews,
-      successRate: '99.4%'
-    };
-
-    await cache.set(cacheKey, stats, 1800);
-
-    res.json({ source: 'database', ...stats });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  if (cachedStats) {
+    return res.json({ source: 'cache', ...cachedStats });
   }
-};
+
+  const totalStudents = await User.countDocuments();
+  const totalCourses = await Course.countDocuments();
+  const totalAssets = await Asset.countDocuments();
+  const totalReviews = await Review.countDocuments();
+
+  const stats = {
+    activeStudents: totalStudents,
+    masterclasses: totalCourses,
+    digitalAssets: totalAssets,
+    totalReviews,
+    successRate: '99.4%'
+  };
+
+  await cache.set(cacheKey, stats, 1800);
+
+  res.json({ source: 'database', ...stats });
+});
 
 module.exports = { getPlatformStats };
